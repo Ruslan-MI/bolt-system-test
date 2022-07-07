2186dbddb7d8dacc20c32bb1c224bf2665f28668
Ruslan-MI <r18.jabber@gmail.com>
2022-07-07 15:14:46 +0300

    Добавляет eslint

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 const OperationType = {
-    BUY: `BUY`,
-    SELL: `SELL`,
+  BUY: 'BUY',
+  SELL: 'SELL',
 };
 
 const getOperationResult = (type, price, quantity) => {
-    switch (type) {
-        case OperationType.BUY:
-            return -price * quantity;
-        case OperationType.SELL:
-            return price * quantity;
-        default:
-            return 0;
-    }
+  switch (type) {
+    case OperationType.BUY:
+      return -price * quantity;
+    case OperationType.SELL:
+      return price * quantity;
+    default:
+      return 0;
+  }
 };
 
 const main = (operations) => {
-    const portfolio = {};
-    let absProfit = 0;
+  const portfolio = {};
+  let absProfit = 0;
 
-    operations.forEach(({
-        type,
-        ticker,
-        price,
-        qnt,
-    }) => {
-        const operationResult = getOperationResult(type, price, qnt);
+  operations.forEach(({
+    type,
+    ticker,
+    price,
+    qnt,
+  }) => {
+    const operationResult = getOperationResult(type, price, qnt);
 
-        absProfit += operationResult;
+    absProfit += operationResult;
 
-        if (!portfolio[ticker]) {
-            portfolio[ticker] = 0;
-        }
+    if (!portfolio[ticker]) {
+      portfolio[ticker] = 0;
+    }
 
-        portfolio[ticker] += operationResult;
-    });
+    portfolio[ticker] += operationResult;
+  });
 
-    return {
-        absProfit,
-        portfolio,
-    };
+  return {
+    absProfit,
+    portfolio,
+  };
 };
 
 module.exports = main;
