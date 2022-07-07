e468dcf6eec6a9bd8b6f1d7e86f0f54a0af7b823
Ruslan-MI <r18.jabber@gmail.com>
2022-07-07 11:55:39 +0300

    Добавляет функцию расчёта прибыли

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
+const OperationType = {
+    BUY: `BUY`,
+    SELL: `SELL`,
+};
+
+const getOperationResult = (type, price, quantity) => {
+    switch (type) {
+        case OperationType.BUY:
+            return -price * quantity;
+        case OperationType.SELL:
+            return price * quantity;
+        default:
+            return 0;
+    }
+};
+
 const main = (operations) => {
-    return null;
-}
+    const portfolio = {};
+    let absProfit = 0;
+
+    operations.forEach(({
+        type,
+        ticker,
+        price,
+        qnt,
+    }) => {
+        const operationResult = getOperationResult(type, price, qnt);
+
+        absProfit += operationResult;
+
+        if (!portfolio[ticker]) {
+            portfolio[ticker] = 0;
+        }
+
+        portfolio[ticker] += operationResult;
+    });
+
+    return {
+        absProfit,
+        portfolio,
+    };
+};
 
 module.exports = main;
